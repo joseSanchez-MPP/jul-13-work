83c3e1de731ae70cba2bb7a2992c8cf679d1011e
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-14 10:07:54 -0500

    Operator Exercise is complete. Still working on the last part of the Constants and Varibales part

---
+++ Jose_S_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//top section containing operators
+let number0 = 15
+let number1 = 12
+
+//middle section doing the operations
+let addedResult = number0 + number1
+let subtractedResult = number0 - number1
+let multipliedResult = number0 * number1
+let dividedResult = number0 / number1
+
+//bottom section printing results
+print("Added \(number0) + \(number1): \(addedResult)")
+print("Subtracted \(number0) - \(number1): \(subtractedResult)")
+print("Multiplied \(number0) * \(number1): \(multipliedResult)")
+print("Divided \(number0) / \(number1): \(dividedResult)")
